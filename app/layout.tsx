581b891fba2b8e4373183d6822161bd7e9296d29
Amin <amin.tareen@gmail.com>
2026-04-30 17:30:08 +0500

    added clarity tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,6 +21,17 @@ export default function RootLayout({
   return (
     <html lang="en">
   <head>
+  {/* 🔥 MICROSOFT CLARITY */}
+  <Script id="clarity" strategy="beforeInteractive">
+    {`
+      (function(c,l,a,r,i,t,y){
+          c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+          t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+          y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+      })(window, document, "clarity", "script", "wjs72cq7ak");
+    `}
+  </Script>
+
   {/* Google Tag (Ads + Analytics Combined) */}
   <Script
     src="https://www.googletagmanager.com/gtag/js?id=AW-18044696705"
