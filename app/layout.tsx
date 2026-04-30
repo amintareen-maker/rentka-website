f8e23ef0f5c5bb97693b45d942b3fc1b983b49d5
Amin <amin.tareen@gmail.com>
2026-04-30 17:35:39 +0500

    clarity final fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,14 +23,14 @@ export default function RootLayout({
   <head>
   {/* 🔥 MICROSOFT CLARITY */}
   <Script id="clarity" strategy="beforeInteractive">
-    {`
-      (function(c,l,a,r,i,t,y){
-          c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-          t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-          y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-      })(window, document, "clarity", "script", "wjs72cq7ak");
-    `}
-  </Script>
+{`
+  (function(c,l,a,r,i,t,y){
+      c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+      t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+      y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+  })(window, document, "clarity", "script", "wjs72cq7ak");
+`}
+</Script>
 
   {/* Google Tag (Ads + Analytics Combined) */}
   <Script
