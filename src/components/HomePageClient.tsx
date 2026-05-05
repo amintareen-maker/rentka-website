8addd315bdb7e37c6343e216b00b7453b514a21b
Amin <amin.tareen@gmail.com>
2026-05-05 18:05:06 +0500

    Update lcar logo back to old

--- src/components/HomePageClient.tsx
+++ src/components/HomePageClient.tsx
@@ -280,7 +280,7 @@ useEffect(() => {
 </h2>
 
 <p className="text-[var(--rentka-blue)">
-  All bookings include a driver. Select your location to see available cars.
+  All bookings are with Driver . Select your location to see multiple options of your preffered car.
 </p>
           </div>
 
