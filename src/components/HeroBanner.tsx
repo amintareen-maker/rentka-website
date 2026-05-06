d588f4bc68025bcde28bff6e1555e168887d2925
Amin <amin.tareen@gmail.com>
2026-05-06 21:22:53 +0500

    Update lcar logo back to old

--- src/components/HeroBanner.tsx
+++ src/components/HeroBanner.tsx
@@ -74,7 +74,7 @@ export default function HeroBanner() {
           {/* LEFT */}
           <div>
             <h1 className="text-4xl md:text-5xl font-bold text-[var(--rentka-blue)] leading-tight">
-              Find the Right Car (withDriver Service) — Without Calling 4 Different Vendors
+              Find the Right Car (withDriver) — Without Calling 4 Different Vendors. 
             </h1>
 
             <p className="mt-6 text-lg text-gray-600 max-w-xl">
