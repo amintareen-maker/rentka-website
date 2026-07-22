ba1bc2cc3f496cf36e16b80352b3cde0f9b201ed
Amin <amin.tareen@gmail.com>
2026-07-22 20:20:19 +0500

    fix redirect loop

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async redirects() {
-    return [
-      {
-        source: "/:path*",
-        has: [
-          {
-            type: "host",
-            value: "www.rentka.co",
-          },
-        ],
-        destination: "https://rentka.co/:path*",
-        permanent: true,
-      },
-    ];
-  },
   images: {
     remotePatterns: [
       {
